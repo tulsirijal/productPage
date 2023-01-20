9f00ca3d454800f32e2ea759054ea8328776155b
tulsirijal2019@gmail.com <tulsirijal@2019>
2023-01-20 18:42:27 +0000

    Added animations

--- toggle.js/script.js
+++ toggle.js/script.js
@@ -4,3 +4,20 @@ function myFunction(){
 hamNav.classList.toggle("ham-nav-list")
 
 }
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    });
+})
+
+
+
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=>observer.observe(el));
